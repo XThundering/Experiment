f1aa8766abb28c63322b554501030f82c91986f6
XThundering <XThundering@qq.com>
2018-10-18 21:40:59 +0800

    do scoop git test

--- main.go
+++ main.go
@@ -3,13 +3,14 @@ package main
 import (
 	"github.com/XThundering/Experiment/plugin"
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 func main() {
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": plugin.Print(),
+		c.JSON(http.StatusOK, gin.H{
+			"return": plugin.Print(),
 		})
 	})
 	r.Run()
